55711959bfb84cf121ca583529f69fd35223904c
Luc1083 <107408006+Luc1083@users.noreply.github.com>
2024-05-24 12:31:07 +0200

    cool

--- CP_CK.m
+++ CP_CK.m
@@ -1,4 +1,4 @@
-function Cp_ck = CP_CK(Sp,T)
+4function Cp_ck = CP_CK(Sp,T)
 % A small function to evaluate thermodynamic data from chemkin database
 % INPUT:
 %     -- Sp: Name of the species
--- main1.m
+++ main1.m
@@ -101,46 +101,46 @@ X1(:,1) = molarfraction(W1, W(:,1), Y1(:,1));
 X2(:,1) = molarfraction(W2, W(:,1), Y2(:,1));
 X3(:,1) = molarfraction(W3, W(:,1), Y3(:,1));
 
-% Begin Simulation
-
-% 1. Fick's Diffusion Model, Binary Diffusion Constants (D_i_carrier)
-
-% Hydrogen is Carrier
-D_H2_H2 = 33.83 * 10^5;
-D_O2_H2 = 18.88 * 10^5;
-D_N2_H2 = 17.76 * 10^5;
-
-% Oxygen is Carrier
-D_H2_O2 = 18.88 * 10^5;
-D_O2_O2 = 5.05 * 10^5;
-D_N2_O2 = 4.96 * 10^5;
- 
-% Nitrogen is Carrier
-D_H2_N2 = 17.76 * 10^5;
-D_O2_N2 = 4.96 * 10^5;
-D_N2_N2 = 4.87 * 10^5;
-
-% 2. Wilke Model, Di_Model2
-
-D1(:,n) = ((X2(:,n) / (D12 * (1-X1(:,n)))) + (X3(:,n) / (D13 * (1-X1(:,n))))).^-1;
-D2(:,n) = ((X1(:,n) / (D21 * (1-X2(:,n)))) + (X3(:,n) / (D23 * (1-X2(:,n))))).^-1;
-D3(:,n) = ((X1(:,n) / (D31 * (1-X3(:,n)))) + (X2(:,n) / (D32 * (1-X3(:,n))))).^-1;
-
-% 3. Le = 1 species diffusivity model (species have same diffusivity as heat)
-
-% Currently n is the time step counter
-% Currently i us the species counter
-
-lambda(:,n) = MixLambda_CK([H2, O2, N2], [X1(:,n), X2(:,n), X3(:,n)], T);
-Cp(:,n) = MixCp_CK([H2, O2, N2], [X1(:,n), X2(:,n), X3(:,n)], T);
-Di_model3 = lambda(:,n) / (rho_m(:,n) * Cp(:,n));
-
-% 4. Le = const number model
-
-Le = [0.3, 1.11, 1.0];
-lambda(:,n) = MixLambda_CK([H2, O2, N2], [X1(:,n), X2(:,n), X3(:,n)], T);
-Cp(:,n) = MixCp_CK([H2, O2, N2], [X1(:,n), X2(:,n), X3(:,n)], T);
-Di_model4 = lamda(:,n) / (Le(i) * rho_m(:,n) * Cp(:,n));
+% % Begin Simulation
+% 
+% % 1. Fick's Diffusion Model, Binary Diffusion Constants (D_i_carrier)
+% 
+% % Hydrogen is Carrier
+% D_H2_H2 = 33.83 * 10^5;
+% D_O2_H2 = 18.88 * 10^5;
+% D_N2_H2 = 17.76 * 10^5;
+% 
+% % Oxygen is Carrier
+% D_H2_O2 = 18.88 * 10^5;
+% D_O2_O2 = 5.05 * 10^5;
+% D_N2_O2 = 4.96 * 10^5;
+% 
+% % Nitrogen is Carrier
+% D_H2_N2 = 17.76 * 10^5;
+% D_O2_N2 = 4.96 * 10^5;
+% D_N2_N2 = 4.87 * 10^5;
+% 
+% % 2. Wilke Model, Di_Model2
+% 
+% D1(:,n) = ((X2(:,n) / (D12 * (1-X1(:,n)))) + (X3(:,n) / (D13 * (1-X1(:,n))))).^-1;
+% D2(:,n) = ((X1(:,n) / (D21 * (1-X2(:,n)))) + (X3(:,n) / (D23 * (1-X2(:,n))))).^-1;
+% D3(:,n) = ((X1(:,n) / (D31 * (1-X3(:,n)))) + (X2(:,n) / (D32 * (1-X3(:,n))))).^-1;
+% 
+% % 3. Le = 1 species diffusivity model (species have same diffusivity as heat)
+% 
+% % Currently n is the time step counter
+% % Currently i us the species counter
+% 
+% lambda(:,n) = MixLambda_CK([H2, O2, N2], [X1(:,n), X2(:,n), X3(:,n)], T);
+% Cp(:,n) = MixCp_CK([H2, O2, N2], [X1(:,n), X2(:,n), X3(:,n)], T);
+% Di_model3 = lambda(:,n) / (rho_m(:,n) * Cp(:,n));
+% 
+% % 4. Le = const number model
+% 
+% Le = [0.3, 1.11, 1.0];
+% lambda(:,n) = MixLambda_CK([H2, O2, N2], [X1(:,n), X2(:,n), X3(:,n)], T);
+% Cp(:,n) = MixCp_CK([H2, O2, N2], [X1(:,n), X2(:,n), X3(:,n)], T);
+% Di_model4 = lamda(:,n) / (Le(i) * rho_m(:,n) * Cp(:,n));
 
 figure('Name','Mole Fractions at t=0')
 hold on 
@@ -151,8 +151,8 @@ legend('show')
 
 figure('Name','Mass Fractions at t=0')
 hold on 
-plot(x,Y1(:,1), 'DisplayName', 'Hydrogen [H2]')
-plot(x,Y2(:,1), 'DisplayName', 'Oxygen [02]')
+plot(x,Y1(:,1), 'DisplayName', 'Hydrogen [H2]') 
+plot(x,Y2(:,1), 'DisplayName', 'Oxygen [02]', 'LineStyle','--')
 plot(x,Y3(:,1), 'DisplayName', 'Nitrogen [N2]')
 legend('show')
 
